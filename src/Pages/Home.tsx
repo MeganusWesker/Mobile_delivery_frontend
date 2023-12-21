ff7f4f280a4e82076d7bf77828a423f810ed6e36
Meganus <mohitjassal157@gmail.com>
2023-12-21 09:02:42 -0800

    layout for mobile

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,8 +6,8 @@ const Home = () => {
     <>
       <Navbar />
       <div className="banner">
-        <h1 className="text-6xl">Welcome to The Site </h1>
-        <p className="text-4xl my-4">FIND AMAZING PRODUCTS BELOW</p>
+        <h1 className="text-6xl max-[600px]:text-4xl">Welcome to The Site </h1>
+        <p className="text-4xl my-4 max-[600px]:text-2xl">FIND AMAZING PRODUCTS BELOW</p>
 
         <a href="#container">
           <button>Scroll</button>
